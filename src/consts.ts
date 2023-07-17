58bb99b5688dfc33ac2a18575f1e81c5579802af
kbario <kylebario1@gmail.com>
2023-07-17 08:26:58 +0800

    styling + play

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Blog - kbar.io';
+export const SITE_DESCRIPTION = 'This is a blog filled with thoughts from yours truely. Topics range from web development and programming, to books I\'ve read, the fitness I do and the hobbies I have. I hope you enjoy.';
